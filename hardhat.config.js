1942857faf59c1e58619756ff5fea65cebd2b631
SreeKrishnan1512 <sreekrrish99@gmail.com>
2024-10-10 18:01:17 +0530

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+require('dotenv').config();
+
+const { TESTNET_RPC, PRIVATE_KEY, API_KEY } = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    arb: {
+      url: TESTNET_RPC,
+      accounts: [PRIVATE_KEY]
+    },
+  },
+  settings: {
+    optimizer: {
+      enabled: true, // Enable optimizer for better gas efficiency
+      runs: 200, // Number of optimization runs
+    },
+  },
+  sourcify: {
+    enabled: true
+  },
+  etherscan: {
+    apiKey: {
+      arbitrumSepolia: API_KEY
+    }
+  }
+};
